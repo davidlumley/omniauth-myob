26a22420b00245a49c77807d44cb2001ca57d207
David Lumley <david@davidlumley.com.au>
2013-10-15 13:40:00 +1000

    initial commit

---
+++ lib/omniauth/myob.rb
@@ -0,0 +1,7 @@
+require "omniauth/myob/version"
+
+module Omniauth
+  module Myob
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/myob/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module Myob
+    VERSION = "0.0.1"
+  end
+end
